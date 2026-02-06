4cb9ce948548e34c35175ea07339ee8e0f870628
dina <dina.hamadeh@edu.esiee.fr>
2026-02-06 15:01:20 +0100

    sit web améliorer mais pas final + scrapper maj + requirement

--- app/app.py
+++ app/app.py
@@ -9,11 +9,10 @@ st.write("This dashboard allows you to explore the data collected from the OpenF
 
 @st.cache_resource
 def get_data_from_mongodb():
-    client = MongoClient("mongodb://localhost:27017/")
-    db = client["openfoodfacts"]
-    collection = db["products"]
-    data = list(collection.find())
-    return data
+    client = MongoClient("mongodb://localhost:27018/")
+    db = client["openfoodfacts_db"]
+    collection = db["products_collection"]
+    return collection
 
 collection = get_data_from_mongodb()
 
@@ -29,3 +28,72 @@ if df.empty:
     st.stop()
     
 st.sidebar.header("Filter Options")
+
+search_name = st.sidebar.text_input("Rechercher un produit")
+
+# Filtre Nutriscore
+nutriscore_options = sorted(df["nutriscore_grade"].dropna().unique())
+selected_nutriscore = st.sidebar.multiselect(
+    "Nutriscore",
+    nutriscore_options
+)
+
+# Filtre NOVA
+nova_options = sorted(df["nova_group"].dropna().unique())
+selected_nova = st.sidebar.multiselect(
+    "NOVA group",
+    nova_options
+)
+
+# Filtre Ecoscore
+ecoscore_options = sorted(df["ecoscore_grade"].dropna().unique())
+selected_ecoscore = st.sidebar.multiselect(
+    "Ecoscore",
+    ecoscore_options
+)
+
+# -----------------------
+# Application des filtres
+# -----------------------
+filtered_df = df.copy()
+
+if search_name:
+    filtered_df = filtered_df[
+        filtered_df["product_name"]
+        .str.contains(search_name, case=False, na=False)
+    ]
+
+if selected_nutriscore:
+    filtered_df = filtered_df[
+        filtered_df["nutriscore_grade"].isin(selected_nutriscore)
+    ]
+
+if selected_nova:
+    filtered_df = filtered_df[
+        filtered_df["nova_group"].isin(selected_nova)
+    ]
+
+if selected_ecoscore:
+    filtered_df = filtered_df[
+        filtered_df["ecoscore_grade"].isin(selected_ecoscore)
+    ]
+
+# -----------------------
+# Résultats
+# -----------------------
+st.subheader("📊 Résultats")
+
+st.write(f"Nombre de produits affichés : **{len(filtered_df)}**")
+
+st.dataframe(
+    filtered_df[
+        [
+            "product_name",
+            "categories_en",
+            "nutriscore_grade",
+            "nova_group",
+            "ecoscore_grade",
+        ]
+    ],
+    use_container_width=True
+)
